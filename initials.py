171306e2a6cb0d3348da5a8908699edec069b243
Princess-Delali <2425404225@live.gctu.edu.gh>
2026-05-31 19:46:07 +0000

    implement my initials

--- initials.py
+++ initials.py
@@ -1,7 +1,32 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name:         Princess Delali Appeh
+# STUDENT ID:   2425404225
+# Email:        2425404225@live.gctu.edu.gh
+# Grader:       Augustus
+#
+# On my honor, Princess Delali Appeh, this programming assignment is my own work
+# and I have not provided this code to any other student.
 
 def main():
-  pass #code goes here
 
+    print("")
+    print("...PDA")
+    print("")
 
+    print("...PPPPPPPP............DDDDDDDD............AAAAAAAAAAAA.....")
+    print("...PPPPPPPPPPP.........DDDDDDDDDD..........AAAAAAAAAAAA.....")
+    print("...PPP......PPP........DD.......DDD........AA........AA.....")
+    print("...PPP......PPP........DD........DD........AA........AA.....")
+    print("...PPPPPPPPPPP.........DD........DD........AAAAAAAAAAAA.....")
+    print("...PPPPPPPP............DD........DD........AAAAAAAAAAAA.....")
+    print("...PPP.................DD........DD........AA........AA.....")
+    print("...PPP.................DD.......DDD........AA........AA.....")
+    print("...PPP............**...DDDDDDDDDD.....**...AA........AA...**")
+    print("...PPP............**...DDDDDDDD.......**...AA........AA...**")
+
+    print("")
 
 main()
